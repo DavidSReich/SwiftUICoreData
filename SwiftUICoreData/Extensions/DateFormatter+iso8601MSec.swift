edd5ceead2b573a15f15585361216563f27d5d94
David Reich <dreich@alum.mit.edu>
2021-01-05 13:21:30 +1100

    Another extension.

---
+++ SwiftUICoreData/Extensions/DateFormatter+iso8601MSec.swift
@@ -0,0 +1,20 @@
+//
+//  DateFormatter+iso8601MSec.swift
+//  SwiftUICoreData
+//
+//  Created by David S Reich on 5/1/21.
+//  Copyright © 2021 Stellar Software Pty Ltd. All rights reserved.
+//
+
+import Foundation
+
+extension DateFormatter {
+  static let iso8601MSec: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+    formatter.calendar = Calendar(identifier: .iso8601)
+    formatter.timeZone = TimeZone(secondsFromGMT: 0)
+    formatter.locale = Locale(identifier: "en_US_POSIX")
+    return formatter
+  }()
+}
